7686ebc3ee54d62201b270a6cdb542cc8e9b92d6
Lexxicus <alexxxicus@gmail.com>
2019-09-17 21:32:02 +0500

    implement articles#create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,17 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    if @article.valid? #проверка на валидность данных
+      @article.save # данные введены корректно, сохранение в бд
+    else
+      render action: 'new' # возвращает страницу, если валидация не прошла
+    end
   end
+  # защищенная передача параметров title и text
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
 end
