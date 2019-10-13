b13cd224b14d1a0506685e2c11b1b9eb0ef0f9f9
Lexxicus <alexxxicus@gmail.com>
2019-10-13 20:54:25 +0500

    add_user name column  to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_04_141945) do
-
-  create_table "abouts", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2019_10_13_155143) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -40,11 +35,6 @@ ActiveRecord::Schema.define(version: 2019_10_04_141945) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "terms", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -53,8 +43,10 @@ ActiveRecord::Schema.define(version: 2019_10_04_141945) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "comments", "articles"
