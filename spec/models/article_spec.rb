e6987fe1650162d3103e63ca78d74bd4ca9f329f
Lexxicus <alexxxicus@gmail.com>
2019-10-31 22:24:35 +0500

    add comment factory

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -19,4 +19,12 @@ describe Article do
     end
   end
 
+  describe "#last_comment" do
+    it "returns the last comment" do
+      article = create(:article_with_comments)
+      #assert
+      expect(article.last_comment.body).to eq "comment body 3"
+    end
+  end
+
 end
