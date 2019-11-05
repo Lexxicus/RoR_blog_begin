5c20ea46dd84fb63c4c65bfa93542049e857e423
Lexxicus <alexxxicus@gmail.com>
2019-11-05 21:22:13 +0500

    add validation for comments body lenght

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :article
   validates :author, presence: true
-  validates :body, presence: true
+  validates :body, length: { in: 6..4000 }, presence: true
 end
