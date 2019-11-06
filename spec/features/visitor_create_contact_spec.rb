2366d0857749d30bc312dd1af05db704239efa39
Lexxicus <alexxxicus@gmail.com>
2019-11-06 20:48:21 +0500

    add account creation tests

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -1,17 +1,19 @@
-require "rails_helper"
+# frozen_string_literal: true
 
-feature "Contact Creation" do
-  scenario "allow a guest to create contact" do
-    visit '/contacts'
-    fill_in 'Email', with: 'user@example.com'
-    fill_in 'Message', with: 'caplin'
+require 'rails_helper'
+
+feature 'Contact Creation' do
+  scenario 'allow a guest to create contact' do
+    visit new_contacts_path
+    fill_in :contact_email, with: 'user@example.com'
+    fill_in :contact_message, with: 'caplin'
     click_button 'Send message'
 
-    expect(page).to have_content 'Thank you'
+    expect(page).to have_content I18n.t('contacts.thank_you')
   end
 
-  scenario "allow acces to contacts page" do
-    visit '/contacts'
+  scenario 'allow acces to contacts page' do
+    visit new_contacts_path
 
     expect(page).to have_content I18n.t('contacts.contact_us')
   end
