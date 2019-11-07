668aaec65f82c4d099fc57dc7385adfc36deaf56
Lexxicus <alexxxicus@gmail.com>
2019-11-07 21:37:27 +0500

    add spec for create article

--- spec/features/user_create_article_spec.rb
+++ spec/features/user_create_article_spec.rb
@@ -10,7 +10,17 @@ feature "Article Creation" do
 
   scenario "allows user to visit article page" do
     visit new_article_path
-    expect(page).to have_content 'New article'
+    expect(page).to have_content I18n.t('article.new_article')
 
   end
+
+  scenario 'allow user to create new article' do
+    visit new_article_path
+    fill_in :article_title, with: 'Article title'
+    fill_in :article_text, with: 'Article text'
+    click_button 'Save Article'
+
+    expect(page).to have_content 'Article title'
+  end
+
 end
